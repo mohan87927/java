fdc03c6879076e8f29c2d1d63b23d504584878d3
Manjunath S <mohanman2266@gmail.com>
2023-03-25 23:44:37 +0530

    conditionla statements in java

---
+++ revision.java
@@ -0,0 +1,20 @@
+class recall{
+    public static void main(String args[])
+    {
+    System.out.println("hello revision session");
+    int a = 1059;
+    byte b = 30;
+    double x = 29;
+    double y = 29;
+    boolean c = y > x && b>a ;
+    // byte d = 1059%256;
+    // byte res = (byte)hello*hey;
+    // System.out.println(d);
+    if (c)
+        System.out.println(c);
+    else 
+        System.out.print("no c");
+
+
+    }
+}
